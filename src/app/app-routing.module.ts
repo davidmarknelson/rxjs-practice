d73232d7775e6a753c449f3a8eb54deb2e6493f3
David Nelson <nelsondavidmark@gmail.com>
2020-03-27 20:35:03 -0400

    Initial commit with sidenav and home page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'projects', loadChildren: () => import('./pages/projects/projects.module').then(m => m.ProjectsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
